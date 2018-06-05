2a859db4a95812f90ac4aa20a3fba422bdcb006b
Benjamin Rich <benlr98@gmail.com>
2018-06-05 13:56:16 -0400

    Updates Spellbook to post-class Tuesday(6/6/18)

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
-function changeTextButton (){
-    const header = document.querySelector('h1')
-    const header2 = document.querySelector('#billy')
-    const header3 = document.querySelector('.dj')
-    header.textContent = 'T-Rex'
-    header2.textContent = 'Bumblebee'
-    header3.textContent = 'Giraffe'
-}
-const changeHeading = function(ev){
-    ev.preventDefault()
-    const f= ev.target
-    const spellName = f.spellName.value
-    document
-        .querySelector('h1#star')
-        .textContent = spellName
-}
-const button = document.querySelector('button')
-const form = document.querySelector('form')
-button.addEventListener('click', changeTextButton)
-form.addEventListener('submit', changeHeading)
 
+const form = document.querySelector('form')
+
+const changeHeading = function(ev) {
+  ev.preventDefault()
+
+  const f = ev.target
+  const spellName = f.spellName.value
+
+  const spellsDiv = document.querySelector('#spells')
+  spellsDiv.innerHTML += `<li>${spellName}</li>`
+
+  f.reset()
+}
+
+form.addEventListener('submit', changeHeading)
