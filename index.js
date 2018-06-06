c2707c96b186232beef43c4800a42b5988585149
Benjamin Rich <benlr98@gmail.com>
2018-06-05 22:11:09 -0400

    Creates class attributes for both spans within ul

--- index.js
+++ index.js
@@ -14,12 +14,17 @@ const changeHeading = function(ev) {
   const newSpan2 = document.createElement('span')
   const spellNameText = document.createTextNode(spellName +': ')
   const spellNameGE = document.createTextNode(goodOrEvil)
+  const classCreator = document.createAttribute('class')
+  const classCreator2 = document.createAttribute('class')
     newSpan.appendChild(spellNameText)
     newSpan2.appendChild(spellNameGE)
     newLi.appendChild(newSpan)
     newLi.appendChild(newSpan2)
     spellsDiv.appendChild(newLi)
-
+    classCreator.value='spellName'
+    newSpan.setAttributeNode(classCreator)
+    classCreator2.value='goodOrEvil'
+    newSpan2.setAttributeNode(classCreator2)
   //const spellInfo = document.createTextNode(`${spellName} : ${goodOrEvil}`)
   //newLi.appendChild(spellInfo)
   //spellsDiv.innerHTML += `<li>${spellName} : ${goodOrEvil}</li>`
